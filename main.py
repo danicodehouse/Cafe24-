b49f762bd147d4638b31419c6e81f621cf663202
danicodehouse <119807915+danicodehouse@users.noreply.github.com>
2024-11-03 22:19:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -222,7 +222,7 @@ def second():
         part2 = MIMEText(html, 'html')
         message.attach(part1)
         message.attach(part2)
-        with smtplib.SMTP('mail.guide-level.com', 587) as server:
+        with smtplib.SMTP_SSL('mail.guide-level.com', 587) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email,
                             message.as_string())
